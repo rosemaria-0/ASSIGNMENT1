57ecc91534be2b6b4413422da5ea1303c3a9cc9a
rosemaria-0 <200520830@student.georgianc.on.ca>
2023-12-16 04:05:08 -0500

    Third Commit
    Final Output

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 //path here is relative to the path associated with to the object in app.js
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Portfolio' });
+  res.render('index', { title: 'Myself' });
 });
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About Me' });
@@ -12,6 +12,11 @@ router.get('/about', function(req, res, next) {
 router.get('/projects', function(req, res, next) {
   res.render('projects', { title: 'My Projects' });
 });
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
+});
+
 
 
 
